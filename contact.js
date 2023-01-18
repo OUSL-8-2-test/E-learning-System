23ec12c6cf698f307bd2edf89d558e78bffc10ea
PawaniIshanka <117011692+PawaniIshanka@users.noreply.github.com>
2023-01-18 05:11:11 -0800

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,9 @@
+function funcClick() {
+    var x = document.getElementById("feed");
+    x.innerHTML = "Wooooow..., Oh!! Really... I am very happy."
+}
+
+function funClick() {
+    var x = document.getElementById("feed");
+    x.innerHTML = "Oops..., Oh!! I am very sad."
+}
